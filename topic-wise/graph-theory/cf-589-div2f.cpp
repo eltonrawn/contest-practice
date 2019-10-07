3a330f819ba52a57c31fff731c7c167188e29e87
Rawnak Sarker <eltonrawn@gmail.com>
2019-10-08 02:09:48 +0600

    solving f yayyy

--- topic-wise/graph-theory/cf-589-div2f.cpp
+++ topic-wise/graph-theory/cf-589-div2f.cpp
@@ -22,11 +22,15 @@ int edge_cnt[140000];
 vector<int> adjl[140000];
 
 vector<int> depth[140000];
+int par_idx[140000];
+
 int func1(int u, int par) {
     int ans = 0;
     FOR(i, 0, (int)adjl[u].size() - 1)   {
         int v = adjl[u][i];
         if(v == par)    {
+            depth[u].PB(0);
+            par_idx[u] = i;
             continue;
         }
         int dep = func1(v, u);
@@ -36,13 +40,39 @@ int func1(int u, int par) {
     return ans;
 }
 
+int func2(int u, int par) {
+    int ans = 0;
+    FOR(i, 0, (int)adjl[u].size() - 1)   {
+        int v = adjl[u][i];
+        if(v == par)    {
+            continue;
+        }
+        int mx = 0;
+        FOR(j, 0, (int)adjl[u].size() - 1)   {
+            if(i == j)    {
+                continue;
+            }
+            mx = max(mx, depth[u][j]);
+        }
+        depth[v][ par_idx[v] ] = mx + 1;
+    }
+    FOR(i, 0, (int)adjl[u].size() - 1)   {
+        int v = adjl[u][i];
+        if(v == par)    {
+            continue;
+        }
+        func2(v, u);
+    }
+    return ans;
+}
+
 int main()  {
-    freopen("in.txt", "r", stdin);
+//    freopen("in.txt", "r", stdin);
 //    freopen("output0.txt", "w", stdout);
     int n;
     cin >> n;
     int node_no = (1 << n) - 1;
-    cout << node_no << "\n";
+//    cout << node_no << "\n";
     FOR(i, 1, node_no - 2)   {
         int u, v;
         cin >> u >> v;
@@ -53,11 +83,22 @@ int main()  {
     }
     FOR(i, 1, node_no)   {
         edge_cnt[ deg[i] ]++;
-        cout << i << " : " << deg[ i ] << "\n";
+//        cout << i << " : " << deg[ i ] << "\n";
     }
 
     int leaf_node_no = (1 << (n - 1));
 
+    if(n == 2)    {
+        cout << n << "\n";
+        FOR(i, 1, 3)   {
+            if(deg[i])    {
+                cout << i << " ";
+            }
+        }
+        return 0;
+    }
+
+    int root;
     /**
     if middle node is cut (from level 3 to level_before_leaf)
     par       edge -> 4, no.of node -> 1
@@ -66,11 +107,37 @@ int main()  {
     non-leaf  edge -> 3, no.of node -> (2 ^ n) - 1 - (2 ^ (n - 1)) - 3
     */
     if((edge_cnt[4] == 1) && (edge_cnt[2] == 1) && (edge_cnt[1] == leaf_node_no) && (edge_cnt[3] == node_no - leaf_node_no - 3))    {
-        cout << 1 << "\n";
+        FOR(i, 1, node_no)   {
+            if(deg[i] == 2)    {
+                root = i;
+                break;
+            }
+        }
+        func1(root, 0);
+
+        int u;///parent node
         FOR(i, 1, node_no)   {
             if(deg[i] == 4)    {
-                cout << i << " ";
+                u = i;
+                break;
             }
+        }
+
+        vector<int> moo;
+        FOR(i, 0, (int)adjl[u].size() - 1)   {
+            if(par_idx[u] == i)    {
+                continue;
+            }
+            moo.PB(depth[u][i]);
+        }
+        sort(moo.begin(), moo.end());
+        if(moo[0] == moo[1] && moo[2] == moo[1] + 1)    {
+            cout << 1 << "\n";
+            cout << u;
+//            if(u == 23261)    {
+//                cout << "hi1";
+//            }
+            return 0;
         }
     }
     /**
@@ -81,41 +148,76 @@ int main()  {
     non-leaf  edge -> 3, no.of node -> (2 ^ n) - 1 - (2 ^ (n - 1)) - 2
     */
     if((edge_cnt[2] == 2) && (edge_cnt[1] == leaf_node_no - 1) && (edge_cnt[3] == node_no - leaf_node_no - 2))    {
-        cout << 1 << "\n";
-        int foo = -1;
+
+        int cut_leaf = -1;
         FOR(u, 1, node_no)   {
             if(deg[u] == 2)    {
+                bool chk = 1;
                 for(int i = 0; i < (int)adjl[u].size(); i++)   {
                     int v = adjl[u][i];
                     if(deg[v] == 1)    {
-                        foo = u;
+                        cut_leaf = u;
+                        chk = 0;
                     }
+                }
+                if(chk)    {
+                    root = u;
                 }
             }
         }
-        cout << foo << "\n";
+        func1(root, 0);
+        if(depth[root][0] == depth[root][1])    {
+            cout << 1 << "\n";
+            cout << cut_leaf << "\n";
+//            if(cut_leaf == 23261)    {
+//                cout << "hi2";
+//            }
+            return 0;
+        }
+
+
     }
     /**
-    if leaf node is cut
+    if root-child node is cut
     leaf      edge -> 1, no.of node -> (2 ^ (n - 1))
     non-leaf  edge -> 3, no.of node -> (2 ^ n) - 1 - (2 ^ (n - 1)) - 1
     */
     if((edge_cnt[1] == leaf_node_no) && (edge_cnt[3] == node_no - leaf_node_no - 1))    {
-        cout << 2 << "\n";
+//        cout << 2 << "\n";
         if(deg[1] != 0)    {
             func1(1, 0);
-            FOR(u, 1, node_no)   {
-                cout << u << " : ";
-                FOR(i, 0, (int)depth[u].size() - 1)   {
-                    int v = depth[u][i];
-                    cout << depth[u][i] << " ";
-                }
-                cout << "\n";
-            }
+            func2(1, 0);
+//            FOR(u, 1, node_no)   {
+//                cout << u << " : ";
+//                FOR(i, 0, (int)depth[u].size() - 1)   {
+//                    int v = depth[u][i];
+//                    cout << depth[u][i] << " ";
+//                }
+//                cout << "\n";
+//            }
         }
         else    {
             func1(2, 0);
+            func2(2, 0);
         }
+        vector<int> foo;
+        FOR(u, 1, node_no)   {
+            if(deg[u] != 3)    {
+                continue;
+            }
+            sort(depth[u].begin(), depth[u].end());
+            if(depth[u][0] == (n - 2) && depth[u][1] == (n - 2) && depth[u][2] == (n - 1))    {
+//                cout << u << " ";
+                foo.PB(u);
+            }
+        }
+        if(foo.size() == 2)    {
+            cout << 2 << "\n";
+            cout << foo[0] << " " << foo[1];
+            return 0;
+        }
+
     }
+    cout << 0;
     return 0;
 }
